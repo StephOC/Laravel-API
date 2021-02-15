5fc18a8e86ad7d8fc74331a8e4bf7fd874352e22
Stephen Ó Connor <stephen.oconnor@kaxmedia.com>
2021-02-15 11:13:59 +0000

    Creating the Register Endpoint

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,28 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/assets/css')
-    .options({
-        processCssUrls: false
-    })
-    .browserSync({
-        proxy: 'localhost:8000',
-        logLevel: 'debug',
-        logPrefix: "GDC-orion",
-        files: [
-            'public/assets/css/**/*.css',
-            './resources/views/**/*.blade.php',
-            './resources/sass/**/*.scss',
-            './resources/js/**/*.js',
-            './resources/js/**/*.vue'
-        ],
-        browser: ["chrome"]
-    });
-
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
