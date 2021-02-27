6662bd2196ca11c78ab6563848343f8e08efadac
Stephen Ó Connor <stephen.oconnor@kaxmedia.com>
2021-02-27 11:24:01 +0000

    Update ArticleTest.php

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\Article;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,6 +19,21 @@ class ArticleTest extends TestCase
     public function testsArticlesAreCreatedCorrectly()
     {
         $user = User::factory()->create();
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+        $article = Article::factory()->create([
+            'title' => 'First Article',
+            'body' => 'First body',
+        ]);
+
+        $payload = [
+            'title' => 'Lorem',
+            'body' => 'Ipsum'
+        ];
+
+        $repsonse = $this->json('PUT',  '/api/articles/' . $article->id, [], $headers)
+            ->assertStatus(204);
+        
     }
 
     public function test_example()
